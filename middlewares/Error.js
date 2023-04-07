1b6058a8427112d306d62ad53ee60e21e937a2d7
Kumar209 <76429886+Kumar209@users.noreply.github.com>
2023-04-07 15:43:37 +0530

    added comment error code

--- middlewares/Error.js
+++ middlewares/Error.js
@@ -14,3 +14,29 @@ const errorMiddleware = (err, req, res ,next) => {
 }
 
 export default errorMiddleware;
+
+
+//Some of the Error Code
+// wrong mongodb id error
+//   if (err.name === "CastError") {
+//     const message = `Resources not found with this id.. Invalid ${err.path}`;
+//     err = new ErrorHandler(message, 400);
+//   }
+
+  // Duplicate key error or Like we have same email in database(email should be unqiue)
+//   if (err.code === 11000) {
+//     const message = `Duplicate key ${Object.keys(err.keyValue)} Entered`;
+//     err = new ErrorHandler(message, 400);
+//   }
+
+  // wrong jwt error
+//   if (err.name === "JsonWebTokenError") {
+//     const message = `Your url is invalid please try again letter`;
+//     err = new ErrorHandler(message, 400);
+//   }
+
+  // jwt expired
+//   if (err.name === "TokenExpiredError") {
+//     const message = `Your Url is expired please try again letter!`;
+//     err = new ErrorHandler(message, 400);
+//   }
